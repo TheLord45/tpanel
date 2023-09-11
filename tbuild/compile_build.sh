4c99bd84b06ead7674b343d5815b5e6b21499dec
Andreas Theofilu <andreas@theosys.at>
2023-09-11 14:03:19 +0200

    Improves build script

--- tbuild/compile_build.sh
+++ tbuild/compile_build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR="${SCRIPT_DIR}/.."
 LOG_FILE="${ROOT_DIR}/workflow.log"
 export QT_DIR="/opt/Qt/6.5.2/gcc_64"
-export QTDIR="${QT_DIR}"
+export CMAKE_PREFIX_PATH="${QT_DIR}/lib/cmake"
 
 # the first parameter is the branch, that is currently being built
 BRANCH="$1"
@@ -31,7 +31,18 @@ fi
 rm -rf "${ROOT_DIR}/build" > /dev/null 2>&1
 mkdir "${ROOT_DIR}/build"
 
+log "Changing to directory $ROOT_DIR"
 cd ${ROOT_DIR}
-cmake -B build -S .
+log "Creating the Makefile"
+cmake -B build -S . >> ${LOG_FILE} 2>&1
+
+if [ $? -ne 0 ]
+then
+    log "ERROR: Could not create Makefile!"
+    exit 1
+fi
+
+log "Changing to build directory"
 cd build
-exec make -j${CPUS}
+log "Building the application ..."
+exec make -j${CPUS} >> ${LOG_FILE} 2>&1
